fddb323dbc4a8a89a8590e9395b91b39aad6ad75
Giulio Marescotti <giulio.marescotti@rgm.it>
2021-01-25 16:27:31 +0100

    feat: added redmine generation feature
    - tool generate textile output to describe DBC (used by redmine)

--- cantools/database/can/c_source.py
+++ cantools/database/can/c_source.py
@@ -28,6 +28,22 @@ Descrizione dei dati del sistema {database_name} V{version}
 
 '''
 
+"""
+rimossa intestazione, ora implementata in redmine
+questo "output" viene assorbito dallo script ps1 e
+incluso in pagina wiki main di redmine. 
+
+h1. Descrizione dei dati del sistema {database_name} V{version}
+
+{description}
+
+{{{{toc}}}}
+"""
+
+REDMINE_HEADER_FMT = '''\
+{nodes_content}
+'''
+
 RST_NODE_FMT = '''\
 ===================
 Modulo {node_name}
@@ -72,6 +88,42 @@ RST_ALARM_TABLE_ROW_FMT = '''\
    | {signal_name:36} | {signal_description:46} |
    +--------------------------------------+------------------------------------------------+
 '''
+
+#########################################
+REDMINE_NODE_FMT = '''\
+
+h2. Modulo {node_name}
+
+{node_description}
+
+'''
+
+REDMINE_SIGNAL_FMT = '''\
+h3. {signal_name}
+
+{signal_description}'''
+   
+REDMINE_SIGNAL_TABLE_FMT = '''\
+
+   |_. Min         |_. Max         |
+   | {signal_min:11} | {signal_max:11} |
+
+'''
+
+REDMINE_ALARM_FMT = '''\
+
+h3. Allarmi
+
+Lista allarmi
+'''
+
+REDMINE_ALARM_TABLE_HEADER_FMT = '''\
+   |_. Alarm                                |_. Description                                    |
+'''
+
+REDMINE_ALARM_TABLE_ROW_FMT = '''\
+   | {signal_name:36} | {signal_description:46} |'''
+########################
 
 QT_HEADER_FMT = '''\
 /**
@@ -1781,6 +1833,71 @@ def _generate_rst_text(database_name, nodes):
     return '\n'.join(nodes_content)
 
 """
+    return:
+        all nodes REDMINE content
+"""
+def _generate_redmine_text(database_name, nodes):
+
+    nodes_content = list()
+
+    for node in nodes:
+        signals_content = list()
+        
+        if not node.commented_signals: continue
+        
+        node_description = list()
+        node_description.append(node.comment)
+        node_description.append("")
+        node_description.append("Il nodo gestisce i seguenti dati:\n")
+        
+        nodes_content.append(REDMINE_NODE_FMT.format(
+            node_name=node.name,
+            node_description='\n'.join(node_description),
+        ))
+        
+        alarms = list()
+        for signal in node.commented_signals:
+            comment = signal.comment.split('\n')
+            signal_name = signal.name.strip() # + "*: _" + comment[0].strip() + "_"
+            signal_description="_" + comment[0] + "_"
+            signal_description+="\n" + ' '.join(comment[1:]).strip()
+        
+            if signal.length == 1: # if length=1 then it is an alarm (assumption...)
+                alarms.append(signal)
+
+            else:
+                nodes_content.append(REDMINE_SIGNAL_FMT.format(
+                    signal_name=signal_name,
+                    signal_description=signal_description,
+                ))
+            
+                if signal._minimum != signal._maximum:
+                    nodes_content.append(REDMINE_SIGNAL_TABLE_FMT.format(
+                        signal_min=pretty_number(signal._minimum, signal.unit),
+                        signal_max=pretty_number(signal._maximum, signal.unit),
+                        ))
+                    
+        if alarms:
+            
+            nodes_content.append(REDMINE_ALARM_FMT.format(
+                signal_name=signal_name,
+                signal_description=signal_description,
+            ))
+            
+            # in alarm table => put header only on first
+            nodes_content.append(REDMINE_ALARM_TABLE_HEADER_FMT.rstrip())
+
+            for signal in alarms:
+                nodes_content.append(REDMINE_ALARM_TABLE_ROW_FMT.rstrip().format(
+                    signal_name=signal.name,
+                    signal_description=signal.comment
+                    ))
+            
+        nodes_content.append("\n".join(signals_content))
+    
+    return '\n'.join(nodes_content)
+
+"""
     return: 
 	signals_classes_declarations x signal
 	signals_variables
@@ -2211,6 +2328,46 @@ def generate_rst(database,
     print(rst_content)
     
     return rst_content
+
+def generate_redmine(database,
+             database_name,
+             encoding):
+    """Generate Textile Text from given CAN database `database`.
+    `database_name` is used as a prefix for all defines, data
+    structures and functions.
+    This function returns the redmine files as strings.
+    """
+
+    date = time.ctime()
+    nodes = database.nodes
+    
+    messages = [Message(message) for message in database.messages]
+    
+    for node in nodes:
+        node.commented_signals = list()
+
+    # Attach to message the node property
+    for msg in messages:
+        msg.node = next(n for n in nodes if n.name == next(iter(msg._senders or []), None))
+        
+        for signal in msg.signals:
+            if (signal.comment):
+                msg.node.commented_signals.append(signal)
+
+    nodes_content = _generate_redmine_text(
+                   database_name,
+                   nodes)
+    
+    redmine_content = REDMINE_HEADER_FMT.format(
+                   version=__version__,
+                   description=database.dbc._comment,
+                   date=date,
+                   database_name=database_name,
+                   nodes_content=nodes_content
+                   )
+
+    print(redmine_content)
+    return redmine_content
 
 def generate_qt(database,
              database_name,
--- cantools/subparsers/generate_c_source.py
+++ cantools/subparsers/generate_c_source.py
@@ -3,7 +3,7 @@ import os
 import os.path
 
 from .. import database
-from ..database.can.c_source import generate_qt, generate, generate_rst
+from ..database.can.c_source import generate_qt, generate, generate_rst, generate_redmine
 from ..database.can.c_source import camel_to_snake_case
 
 
@@ -70,6 +70,23 @@ def _do_generate_pdf_source(args):
     else:
         # print('Successfully generated {} and {}.'.format(filename_rst, filename_pdf))
         print('Successfully generated {}.'.format(filename_rst))
+
+def _do_generate_redmine_source(args):
+    dbase = database.load_file(args.infile,
+                               encoding=args.encoding,
+                               strict=not args.no_strict)
+
+    if args.database_name is None:
+        basename = os.path.basename(args.infile)
+        database_name = os.path.splitext(basename)[0]
+        database_name = camel_to_snake_case(database_name)
+    else:
+        database_name = args.database_name
+
+    redmine_content = generate_redmine(
+        dbase,
+        database_name,
+        args.encoding)
 
 def _do_generate_c_source(args):
     dbase = database.load_file(args.infile,
@@ -170,6 +187,7 @@ def add_subparser(subparsers):
 
     add_qt_subparser(subparsers)
     add_pdf_subparser(subparsers)
+    add_redmine_subparser(subparsers)
 
 def add_qt_subparser(subparsers):
     generate_qt_source = subparsers.add_parser(
@@ -224,3 +242,22 @@ def add_pdf_subparser(subparsers):
         'infile',
         help='Input database file.')
     generate_pdf_source.set_defaults(func=_do_generate_pdf_source)
+
+def add_redmine_subparser(subparsers):
+    generate_redmine_source = subparsers.add_parser(
+        'generate_redmine',
+        description='Generate documentation in REDMINE for the given database file.')
+    generate_redmine_source.add_argument(
+        '--database-name',
+        help='The database name (default: input file name).')
+    generate_redmine_source.add_argument(
+        '-e', '--encoding',
+        help='File encoding.')
+    generate_redmine_source.add_argument(
+        '--no-strict',
+        action='store_true',
+        help='Skip database consistency checks.')
+    generate_redmine_source.add_argument(
+        'infile',
+        help='Input database file.')
+    generate_redmine_source.set_defaults(func=_do_generate_redmine_source)
